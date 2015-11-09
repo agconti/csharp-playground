4daa434fe13c1df14b67356f11ddbad3b314baab
agconti <agc11d@gmail.com>
2015-11-09 13:32:39 -0500

    feat(Scripts): basic declartions and logging statements
    https://youtu.be/WQnpeR7u7HM

---
+++ Assets/Scripts/declarations_and_logging.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class declarations_and_logging : MonoBehaviour {
+
+	int myInt = 5;
+	int val;
+
+	void Start () {
+		val = MultiplyByTwo (myInt);
+		Debug.Log (val);
+	}
+
+	int MultiplyByTwo(int number) {
+		int val;
+		val = number * 2;
+
+		return val;
+	}
+}
