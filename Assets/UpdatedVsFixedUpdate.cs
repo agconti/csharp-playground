a1ea9eef8f36953a94baada559623bdcb51660c9
agconti <agc11d@gmail.com>
2015-11-09 14:24:42 -0500

    feat(scripts): update vs fixed update

---
+++ Assets/UpdatedVsFixedUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+	/* Called at a regular interval independent of framerate. All physicis 
+	 * calculations should be done here. 
+	 */
+	void FixedUpdate ()
+	{
+		Debug.Log("FixedUpdate time :" + Time.deltaTime);
+	}
+	
+
+	/* called once per frame. Frames will take diffrent times to process depending on
+	 * what needs to be rendered in that frame. The result is that this print statement 
+	 * will log at irregular intervals.
+	 */
+	void Update ()
+	{
+		Debug.Log("Update time :" + Time.deltaTime);
+	}
+}
