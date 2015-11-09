8ed10f3bc9221a8585f5590a12f7edf1d86d5202
agconti <agc11d@gmail.com>
2015-11-09 14:17:47 -0500

    feat(Scirpts): awake and start
    https://youtu.be/M1KuIi5pCno

---
+++ Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AwakeAndStart : MonoBehaviour {
+
+	void Awake () {
+		print ("I've been intialized, but nothing has happend yet. I might not even be enabled!");
+	}
+
+	void Start () {
+		print ("I'm enabled, and ready to do things!");
+	}
+}
